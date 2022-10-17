552910874eb04a98c01347306c45e00f75d7a606
韩云智 <hanyzjob@163.com>
2022-10-17 15:39:03 +0800

    git auto commit master

---
+++ PixelGame/PixelGameLite/RewardedAdManager.swift
@@ -0,0 +1,109 @@
+//
+//  AppRewardedAdManager.swift
+//  PixelGameLite
+//
+//  Created by 韩云智 on 2022/10/17.
+//
+
+import GoogleMobileAds
+
+class RewardedAdManager: NSObject {
+    var rewardedAd: GADRewardedAd?
+    var handler: (() -> Void)?
+    
+    let timeoutInterval: TimeInterval = 4 * 3_600
+    var isLoadingAd = false
+    var isShowingAd = false
+    var loadTime: Date?
+    
+    static let shared = RewardedAdManager()
+    
+    private func wasLoadTimeLessThanNHoursAgo(timeoutInterval: TimeInterval) -> Bool {
+        // Check if ad was loaded more than n hours ago.
+        if let loadTime = loadTime {
+            return Date().timeIntervalSince(loadTime) < timeoutInterval
+        }
+        return false
+    }
+    
+    private func isAdAvailable() -> Bool {
+        // Check if ad exists and can be shown.
+        return rewardedAd != nil && wasLoadTimeLessThanNHoursAgo(timeoutInterval: timeoutInterval)
+    }
+    
+    func loadAd() {
+        // Do not load ad if there is an unused ad or one is already loading.
+        if isLoadingAd || isAdAvailable() {
+            return
+        }
+        isLoadingAd = true
+        debugPrint("Start loading app open ad.")
+        GADRewardedAd.load(
+            withAdUnitID: adRewardedKey,
+            request: GADRequest()
+        ) { (ad, error) in
+            self.isLoadingAd = false
+            if let error = error {
+                self.rewardedAd = nil
+                self.loadTime = nil
+                debugPrint("Rewarded ad failed to load with error: \(error.localizedDescription)")
+                return
+            }
+            debugPrint("Loading Succeeded")
+            self.rewardedAd = ad
+            self.rewardedAd?.fullScreenContentDelegate = self
+            self.loadTime = Date()
+            if let handler = self.handler {
+                self.handler = nil
+                self.showAdIfAvailable(handler)
+            }
+        }
+    }
+    
+    func showAdIfAvailable(_ handler: @escaping () -> Void) {
+        // If the app open ad is already showing, do not show the ad again.
+        if isShowingAd {
+            debugPrint("App open ad is already showing.")
+            return
+        }
+        // If the app open ad is not available yet but it is supposed to show,
+        // it is considered to be complete in this example. Call the appOpenAdManagerAdDidComplete
+        // method and load a new ad.
+        if !isAdAvailable() {
+            debugPrint("App open ad is not ready yet.")
+            self.handler = handler
+            loadAd()
+            return
+        }
+        if let ad = rewardedAd, let rootViewController = UIApplication.shared.windows.first(where: { $0.isKeyWindow })?.rootViewController {
+            debugPrint("App open ad will be displayed.")
+            isShowingAd = true
+            ad.present(fromRootViewController: rootViewController) {
+                handler()
+            }
+        }
+    }
+}
+
+extension RewardedAdManager: GADFullScreenContentDelegate {
+    func adWillPresentFullScreenContent(_ ad: GADFullScreenPresentingAd) {
+        debugPrint("App open ad is will be presented.")
+    }
+    
+    func adDidDismissFullScreenContent(_ ad: GADFullScreenPresentingAd) {
+        rewardedAd = nil
+        isShowingAd = false
+        debugPrint("App open ad was dismissed.")
+        loadAd()
+    }
+    
+    func ad(
+        _ ad: GADFullScreenPresentingAd,
+        didFailToPresentFullScreenContentWithError error: Error
+    ) {
+        rewardedAd = nil
+        isShowingAd = false
+        debugPrint("App open ad failed to present with error: \(error.localizedDescription).")
+        loadAd()
+    }
+}
