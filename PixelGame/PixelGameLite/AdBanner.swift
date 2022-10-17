e4979920fde58ed297be6edb7c7e05185fc139cc
韩云智 <hanyzjob@163.com>
2022-10-17 13:59:08 +0800

    git auto commit master

--- PixelGame/PixelGameLite/AdBanner.swift
+++ PixelGame/PixelGameLite/AdBanner.swift
@@ -17,44 +17,44 @@ struct AdBanner: UIViewControllerRepresentable {
         bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
         bannerView.rootViewController = uiViewController
         view.addSubview(bannerView)
-        view.addConstraints([
-            NSLayoutConstraint(
-                item: bannerView,
-                attribute: .top,
-                relatedBy: .equal,
-                toItem: view,
-                attribute: .top,
-                multiplier: 1,
-                constant: 0
-            ),
-            NSLayoutConstraint(
-                item: bannerView,
-                attribute: .bottom,
-                relatedBy: .equal,
-                toItem: view,
-                attribute: .bottom,
-                multiplier: 1,
-                constant: 0
-            ),
-            NSLayoutConstraint(
-                item: bannerView,
-                attribute: .leading,
-                relatedBy: .equal,
-                toItem: view,
-                attribute: .leading,
-                multiplier: 1,
-                constant: 0
-            ),
-            NSLayoutConstraint(
-                item: bannerView,
-                attribute: .trailing,
-                relatedBy: .equal,
-                toItem: view,
-                attribute: .trailing,
-                multiplier: 1,
-                constant: 0
-            )
-        ])
+//        view.addConstraints([
+//            NSLayoutConstraint(
+//                item: bannerView,
+//                attribute: .top,
+//                relatedBy: .equal,
+//                toItem: view,
+//                attribute: .top,
+//                multiplier: 1,
+//                constant: 0
+//            ),
+//            NSLayoutConstraint(
+//                item: bannerView,
+//                attribute: .bottom,
+//                relatedBy: .equal,
+//                toItem: view,
+//                attribute: .bottom,
+//                multiplier: 1,
+//                constant: 0
+//            ),
+//            NSLayoutConstraint(
+//                item: bannerView,
+//                attribute: .leading,
+//                relatedBy: .equal,
+//                toItem: view,
+//                attribute: .leading,
+//                multiplier: 1,
+//                constant: 0
+//            ),
+//            NSLayoutConstraint(
+//                item: bannerView,
+//                attribute: .trailing,
+//                relatedBy: .equal,
+//                toItem: view,
+//                attribute: .trailing,
+//                multiplier: 1,
+//                constant: 0
+//            )
+//        ])
         return uiViewController
     }
     func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
