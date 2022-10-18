d3f2fe0fbc9be8398bd602a48de84b1f489d7a49
韩云智 <hanyzjob@163.com>
2022-10-18 11:22:21 +0800

    git auto commit master

--- PixelGame/Public/SourceCode/Views/XS_Grid.swift
+++ PixelGame/Public/SourceCode/Views/XS_Grid.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct XS_Grid: View {
+    @Binding var points: [[XS_Point]]
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
