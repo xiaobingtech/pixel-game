cefec8c8eff0a6ccff7ccbcf8b694d7f30fc6454
韩云智 <hanyzjob@163.com>
2022-10-18 14:45:16 +0800

    git auto commit master

--- PixelGame/Public/SourceCode/Views/XS_Grid.swift
+++ PixelGame/Public/SourceCode/Views/XS_Grid.swift
@@ -21,15 +21,37 @@ struct XS_Grid: View {
     
     private func offsetBtn(_ direction: DirectionType) -> some View {
         Button {
-            
+            let offset = CGFloat(options.count/2)
+            switch direction {
+            case .up: options.offset.y += offset
+            case .down: options.offset.y -= offset
+            case .left: options.offset.x -= offset
+            case .right: options.offset.x += offset
+            }
         } label: {
-            Image(systemName: "chevron.up.circle")
+            Image(systemName: "chevron." + direction.rawValue + ".circle")
+                .font(.largeTitle)
+                .foregroundColor(Color(uiColor: .label))
+                .opacity(0.4)
         }
     }
     private var content: some View {
-        VStack {
-            HStack {
-                
+        GeometryReader { proxy in
+            ZStack {
+                offsetBtn(.up)
+                    .frame(maxHeight: .infinity, alignment: .top)
+                offsetBtn(.down)
+                    .frame(maxHeight: .infinity, alignment: .bottom)
+                offsetBtn(.left)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                offsetBtn(.right)
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                ZStack {
+                    let size = proxy.size.width/Double(options.count)
+                    
+                    
+                    
+                }
             }
         }
     }
@@ -38,6 +60,7 @@ struct XS_Grid: View {
         GeometryReader { proxy in
             let size = min(proxy.size.width, proxy.size.height)
             content
+                .padding()
                 .frame(width: size, height: size)
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
         }
