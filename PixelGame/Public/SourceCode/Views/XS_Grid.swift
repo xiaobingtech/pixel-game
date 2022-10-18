dde5c3c0a6189cc272bee70566c901292c2e521f
韩云智 <hanyzjob@163.com>
2022-10-18 11:22:14 +0800

    git auto commit master

---
+++ PixelGame/Public/SourceCode/Views/XS_Grid.swift
@@ -0,0 +1,20 @@
+//
+//  XS_Grid.swift
+//  PixelGame
+//
+//  Created by 韩云智 on 2022/10/18.
+//
+
+import SwiftUI
+
+struct XS_Grid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct XS_Grid_Previews: PreviewProvider {
+    static var previews: some View {
+        XS_Grid()
+    }
+}
