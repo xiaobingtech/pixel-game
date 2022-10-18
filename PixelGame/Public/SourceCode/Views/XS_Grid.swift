a642307202ef04c2f1a974783de36254b41f7fdd
韩云智 <hanyzjob@163.com>
2022-10-18 13:42:40 +0800

    git auto commit master

--- PixelGame/Public/SourceCode/Views/XS_Grid.swift
+++ PixelGame/Public/SourceCode/Views/XS_Grid.swift
@@ -11,8 +11,7 @@ struct XS_Grid: View {
     let color: CGColor
     @Binding var points: [[XS_Point]]
     @Binding var options: XS_Options
-    #if isLite
-    #endif
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
