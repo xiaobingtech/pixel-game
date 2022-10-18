40d067c5f383d020ad4f5bc0cd9d52728779e53f
韩云智 <hanyzjob@163.com>
2022-10-18 09:50:06 +0800

    git auto commit master

--- PixelGame/Public/SourceCode/Views/XS_Root.swift
+++ PixelGame/Public/SourceCode/Views/XS_Root.swift
@@ -11,20 +11,16 @@ struct XS_Root: View {
     @Environment(\.xs_hud) private var xs_hud
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            .onTapGesture {
-                xs_hud.showToast("DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(3))")
-                xs_hud.isActivity = true
-                DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(3)) {
-                    xs_hud.isActivity = false
-                }
-            }
     }
+}
+
+struct XS_Point: Equatable {
+    let position: CGPoint
+    var color: CGColor
 }
 
 struct XS_Root_Previews: PreviewProvider {
     static var previews: some View {
-        XS_Hud {
-            XS_Root()
-        }
+        XS_Root()
     }
 }
