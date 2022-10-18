a1ecb771ba269cad99cd1f6dfcbb9b165fbb7240
韩云智 <hanyzjob@163.com>
2022-10-18 13:46:36 +0800

    git auto commit master

--- PixelGame/PixelGameLite/AppDelegate.swift
+++ PixelGame/PixelGameLite/AppDelegate.swift
@@ -8,9 +8,15 @@
 import UIKit
 import GoogleMobileAds
 
+#if DEBUG
 let adOpenKey: String = "ca-app-pub-3940256099942544/5662855259"
 let adBannerKey: String = "ca-app-pub-3940256099942544/2934735716"
 let adRewardedKey: String = "ca-app-pub-3940256099942544/1712485313"
+#else
+let adOpenKey: String = "ca-app-pub-3940256099942544/5662855259"
+let adBannerKey: String = "ca-app-pub-3940256099942544/2934735716"
+let adRewardedKey: String = "ca-app-pub-3940256099942544/1712485313"
+#endif
 
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
