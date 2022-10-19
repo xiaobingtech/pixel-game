04c3850321eb9ae62cdc9e16017af643463fb162
韩云智 <hanyzjob@163.com>
2022-10-19 14:08:20 +0800

    git auto commit master

--- PixelGame/Public/SourceCode/Views/XS_Root.swift
+++ PixelGame/Public/SourceCode/Views/XS_Root.swift
@@ -15,7 +15,9 @@ struct XS_Root: View {
     @State private var options: XS_Options = .init()
     @State private var color: CGColor = UIColor.black.cgColor
     
-    private var save:
+    private var save: some View {
+        Button {
+    }
     
     private var menu: some View {
         HStack {
