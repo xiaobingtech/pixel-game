118ef7dacb4b0072ffe42a5dec69862de442fcae
韩云智 <hanyzjob@163.com>
2022-10-19 15:55:49 +0800

    git auto commit master

--- PixelGame/Public/SourceCode/Shared/Tools/XS_Tools.swift
+++ PixelGame/Public/SourceCode/Shared/Tools/XS_Tools.swift
@@ -19,15 +19,9 @@ extension UIApplication {
 
 extension String {
     var md5: String {
-        const char *cStr = [str UTF8String];
-            unsigned char result[16];
-            CC_MD5(cStr, strlen(cStr), result); // This is the md5 call
-            return [NSString stringWithFormat:
-                    @"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
-                    result[0], result[1], result[2], result[3],
-                    result[4], result[5], result[6], result[7],
-                    result[8], result[9], result[10], result[11],
-                    result[12], result[13], result[14], result[15]
-                    ];
+        let utf8 = cString(using: .utf8)
+        var digest = [UInt8](repeating: 0, count: Int(CC_MD5_DIGEST_LENGTH))
+        CC_MD5(utf8, CC_LONG(utf8!.count - 1), &digest)
+        return digest.reduce("") { $0 + String(format: "%02x", $1) }
     }
 }
