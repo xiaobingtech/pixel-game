883aa9cd21713b85b82c242bbcc50daa62a751c8
韩云智 <hanyzjob@163.com>
2022-10-19 15:50:11 +0800

    git auto commit master

--- PixelGame/Public/SourceCode/Shared/Tools/XS_Tools.swift
+++ PixelGame/Public/SourceCode/Shared/Tools/XS_Tools.swift
@@ -5,4 +5,13 @@
 //  Created by 韩云智 on 2022/10/19.
 //
 
-import Foundation
+import UIKit
+
+extension UIApplication {
+    static var keyWindow: UIWindow? {
+        (UIApplication.shared.connectedScenes
+            .filter { $0.activationState == .foregroundActive }
+            .first { $0 is UIWindowScene } as? UIWindowScene)?.windows
+            .first { $0.isKeyWindow }
+    }
+}
