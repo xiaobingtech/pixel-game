67542093e6fd941f8fe4c12a88b19a89cb97837c
韩云智 <hanyzjob@163.com>
2022-10-19 16:53:00 +0800

    git auto commit master

--- PixelGame/Public/SourceCode/Views/XS_Root.swift
+++ PixelGame/Public/SourceCode/Views/XS_Root.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 import SwiftHash
 
-let topicFilePath = NSHomeDirectory() + "/Library/Caches/topic"
+let filePath = NSHomeDirectory() + "/Library/Caches/topic"
 
 struct XS_Root: View {
     @Environment(\.xs_hud) private var xs_hud
