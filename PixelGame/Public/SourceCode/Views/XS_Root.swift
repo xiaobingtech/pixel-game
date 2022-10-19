b35230082ac12fad2d38f9776494d0176c519754
韩云智 <hanyzjob@163.com>
2022-10-19 15:07:29 +0800

    git auto commit master

--- PixelGame/Public/SourceCode/Views/XS_Root.swift
+++ PixelGame/Public/SourceCode/Views/XS_Root.swift
@@ -14,11 +14,7 @@ struct XS_Root: View {
     @State private var points: [[XS_Point]] = [[]]
     @State private var options: XS_Options = .init()
     @State private var color: CGColor = UIColor.black.cgColor
-    @State private var bgColor: CGColor!
-    
-    init() {
-        _bgColor = UIColor.systemBackground.cgColor
-    }
+    @State private var bgColor: CGColor?
     
     private var open: some View {
         Button {
