9aa1096737a99beb3ad27d6790ab0996caf265f3
韩云智 <hanyzjob@163.com>
2022-10-19 17:42:33 +0800

    git auto commit master

--- PixelGame/Public/SourceCode/Shared/Tools/XS_Tools.swift
+++ PixelGame/Public/SourceCode/Shared/Tools/XS_Tools.swift
@@ -15,3 +15,27 @@ extension UIApplication {
             .first { $0.isKeyWindow }
     }
 }
+
+//产生公/私钥
+//
+//// 私钥
+//let privateKey = Curve25519.Signing.PrivateKey()
+//// 公钥
+//let publicKey = privateKey.publicKey
+//// 发布公钥
+//let publicKeyData = publicKey.rawRepresentation
+//
+//私钥签名
+//
+//let str = "Hello CryptoKit"
+//let data = str.data(using: .utf8)!
+//
+//let signature = try? privateKey.signature(for: data)
+//
+//公钥验证
+//
+//if let signature = signature {
+//    if publicKey.isValidSignature(NSData(data: signature) , for: data) {
+//        print("签名有效")
+//    }
+//}
