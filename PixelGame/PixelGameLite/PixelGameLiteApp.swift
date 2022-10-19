41b6c01bd782dde58160f75dd90591d79c33e902
韩云智 <hanyzjob@163.com>
2022-10-19 15:49:42 +0800

    git auto commit master

--- PixelGame/PixelGameLite/PixelGameLiteApp.swift
+++ PixelGame/PixelGameLite/PixelGameLiteApp.swift
@@ -7,15 +7,6 @@
 
 import SwiftUI
 
-extension UIApplication {
-    static var keyWindow: UIWindow? {
-        (UIApplication.shared.connectedScenes
-            .filter { $0.activationState == .foregroundActive }
-            .first { $0 is UIWindowScene } as? UIWindowScene)?.windows
-            .first { $0.isKeyWindow }
-    }
-}
-
 @main
 struct PixelGameLiteApp: App {
     @UIApplicationDelegateAdaptor private var appDelegate: AppDelegate
