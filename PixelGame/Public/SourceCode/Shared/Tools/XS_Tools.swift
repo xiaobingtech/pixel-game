9a7814a9465f13a062a515f3c84a2665ca51a359
韩云智 <hanyzjob@163.com>
2022-10-19 16:11:56 +0800

    git auto commit master

--- PixelGame/Public/SourceCode/Shared/Tools/XS_Tools.swift
+++ PixelGame/Public/SourceCode/Shared/Tools/XS_Tools.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import CommonCrypto
 
 extension UIApplication {
     static var keyWindow: UIWindow? {
@@ -14,30 +13,5 @@ extension UIApplication {
             .filter { $0.activationState == .foregroundActive }
             .first { $0 is UIWindowScene } as? UIWindowScene)?.windows
             .first { $0.isKeyWindow }
-    }
-}
-
-extension String {
-//    var md5: String {
-//        let utf8 = cString(using: .utf8)
-//        var digest = [UInt8](repeating: 0, count: Int(CC_MD5_DIGEST_LENGTH))
-//        CC_MD5(utf8, CC_LONG(utf8!.count - 1), &digest)
-//        return digest.reduce("") { $0 + String(format: "%02x", $1) }
-//    }
-    var md5: Data {
-        let length = Int(CC_MD5_DIGEST_LENGTH)
-        let messageData = data(using:.utf8)!
-        var digestData = Data(count: length)
-
-                _ = digestData.withUnsafeMutableBytes { digestBytes -> UInt8 in
-                    messageData.withUnsafeBytes { messageBytes -> UInt8 in
-                        if let messageBytesBaseAddress = messageBytes.baseAddress, let digestBytesBlindMemory = digestBytes.bindMemory(to: UInt8.self).baseAddress {
-                            let messageLength = CC_LONG(messageData.count)
-                            CC_MD5(messageBytesBaseAddress, messageLength, digestBytesBlindMemory)
-                        }
-                        return 0
-                    }
-                }
-                return digestData
     }
 }
