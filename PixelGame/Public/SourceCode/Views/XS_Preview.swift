c085dfb92c54baf6384ed586e9be9d54fe3be747
韩云智 <hanyzjob@163.com>
2022-10-19 14:47:56 +0800

    git auto commit master

--- PixelGame/Public/SourceCode/Views/XS_Preview.swift
+++ PixelGame/Public/SourceCode/Views/XS_Preview.swift
@@ -17,9 +17,9 @@ struct XS_Preview: View {
     private var theScene: SCNScene {
         let scene = SCNScene()
         scene.background.contents = color
-        colorScheme == .dark
-        ? UIColor.black
-        : UIColor.white
+//        colorScheme == .dark
+//        ? UIColor.black
+//        : UIColor.white
         
         for (index, arr) in points.enumerated() {
             for point in arr {
