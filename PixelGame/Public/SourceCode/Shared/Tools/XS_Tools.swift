ccfaf0d4ea20b6d26102695cf9a9c8216e3214e9
韩云智 <hanyzjob@163.com>
2022-10-20 10:52:59 +0800

    git auto commit master

--- PixelGame/Public/SourceCode/Shared/Tools/XS_Tools.swift
+++ PixelGame/Public/SourceCode/Shared/Tools/XS_Tools.swift
@@ -39,8 +39,7 @@ struct XS_Tools {
             let data = try JSONEncoder().encode(points)
             let str = try safe(String(data: data, encoding: .utf8))
             let md5 = MD5(str)
-            var myData = Data()
-            myData.count = 256
+            var myData = Data(repeating: 0, count: 256)
             let key = SymmetricKey(data: myData)
             
             
