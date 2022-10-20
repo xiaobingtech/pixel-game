4fcebb11d12208fedf9096110b30408b0b8ee873
韩云智 <hanyzjob@163.com>
2022-10-20 09:05:01 +0800

    git auto commit master

--- PixelGame/Public/SourceCode/Shared/Tools/XS_Tools.swift
+++ PixelGame/Public/SourceCode/Shared/Tools/XS_Tools.swift
@@ -19,7 +19,15 @@ extension UIApplication {
 struct XS_Tools {
     static func save(_ points: [[XS_Point]]) {
         print(points)
-        let new = points.map { $0.sorted { $0.position.x < $1.position.x || $0.position.y < $1.position.y } }
+        let new = points.map {
+            $0.sorted {
+                if $0.position.x == $1.position.x {
+                    return $0.position.y < $1.position.y
+                } else {
+                    return $0.position.x < $1.position.x
+                }
+            }
+        }
         print(new)
     }
 }
