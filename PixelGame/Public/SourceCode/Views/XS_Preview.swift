e6940935e54964cc22b0199be79eed8f25d4e8f8
韩云智 <hanyzjob@163.com>
2022-10-22 16:29:32 +0800

    git auto commit master

--- PixelGame/Public/SourceCode/Views/XS_Preview.swift
+++ PixelGame/Public/SourceCode/Views/XS_Preview.swift
@@ -53,15 +53,16 @@ struct XS_Preview: View {
         let camera = SCNCamera()
         let cameraNode = SCNNode()
         cameraNode.camera = camera
+        let maxL = max(Float(maxPoint.x - minPoint.x), Float(maxPoint.y - minPoint.y), Float(points.count))
         cameraNode.position = SCNVector3(
             x: Float(minPoint.x + maxPoint.x)/2,
             y: Float(points.count)/2 - 0.5,
-            z: Float(maxPoint.y + 2 + (maxPoint.x - minPoint.x)/2) + 20
+            z: Float(minPoint.y + maxPoint.y)/2 + maxL/2 + 20.5
         )
         scene.rootNode.addChildNode(cameraNode)
         
-        camera.zFar
-        camera.zNear
+        camera.zFar = 21 + Double(maxL)
+        camera.zNear = 1
         
         return scene
     }
