b348206dddc66b32321caa6d77c0da084cb7fa0e
韩云智 <hanyzjob@163.com>
2022-10-27 17:43:41 +0800

    git auto commit 1.0.1

--- PixelGame/Public/SourceCode/Views/XS_Others.swift
+++ PixelGame/Public/SourceCode/Views/XS_Others.swift
@@ -27,7 +27,7 @@ struct XS_Others: View {
                 Spacer()
                 Image(systemName: "chevron.right")
             }
-            .font(.system(size: 30))
+            .font(.system(size: 20))
             .foregroundColor(Color(uiColor: .label))
             .frame(height: 50)
         }
