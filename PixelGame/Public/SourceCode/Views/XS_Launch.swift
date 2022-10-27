5d28b3cfd6c11ce5c4259822ad1d61a6958627c8
韩云智 <hanyzjob@163.com>
2022-10-27 15:56:33 +0800

    git auto commit 1.0.1

---
+++ PixelGame/Public/SourceCode/Views/XS_Launch.swift
@@ -0,0 +1,20 @@
+//
+//  XS_Launch.swift
+//  PixelGame
+//
+//  Created by 韩云智 on 2022/10/27.
+//
+
+import SwiftUI
+
+struct XS_Launch: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct XS_Launch_Previews: PreviewProvider {
+    static var previews: some View {
+        XS_Launch()
+    }
+}
