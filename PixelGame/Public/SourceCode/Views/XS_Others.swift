6f09424d2c2b84e8319695f758342f964fdbbac4
韩云智 <hanyzjob@163.com>
2022-10-27 15:32:22 +0800

    git auto commit 1.0.1

--- PixelGame/Public/SourceCode/Views/XS_Others.swift
+++ PixelGame/Public/SourceCode/Views/XS_Others.swift
@@ -15,7 +15,7 @@ struct XS_Others: View {
         ZStack(alignment: .topTrailing) {
             ScrollView(.vertical, showsIndicators: false) {
                 VStack {
-                    Image("AppIcon60x60")
+                    Image("AppIcon1024x1024")
                         .resizable()
                         .frame(width: 80, height: 80)
                 }
