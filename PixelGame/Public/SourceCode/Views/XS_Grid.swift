4ad8ef0143595ec412faca0fbe2fa9e014ad52fb
韩云智 <hanyzjob@163.com>
2022-10-28 16:03:15 +0800

    git auto commit 1.0.1

--- PixelGame/Public/SourceCode/Views/XS_Grid.swift
+++ PixelGame/Public/SourceCode/Views/XS_Grid.swift
@@ -227,12 +227,12 @@ struct XS_Grid: View {
                 s = (size.width - width)/2
                 point = CGPoint(x: s/2, y: s/2)
             } else {
-                s = (size.height - width)/2
+                s = (size.height - width)/2 + 2
                 point = CGPoint(x: s/2, y: s/2)
             }
             return content
                 .frame(width: s, height: s)
-//                .position(point)
+                .position(point)
         }
     }
 }
