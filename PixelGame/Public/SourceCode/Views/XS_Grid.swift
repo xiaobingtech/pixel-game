f6cf6483888ccc946fa570db71931276827c4efd
韩云智 <hanyzjob@163.com>
2022-11-01 20:26:02 +0800

    git auto commit 1.0.1

--- PixelGame/Public/SourceCode/Views/XS_Grid.swift
+++ PixelGame/Public/SourceCode/Views/XS_Grid.swift
@@ -220,7 +220,7 @@ struct XS_Grid: View {
                     }
                 }
                 .frame(width: size.width, height: size.height)
-                .offset(x: -center.x, y: -center.y)
+                .offset(x: -center.x - 1, y: -center.y + 1)
                 .offset(x: size.width/2, y: size.height/2)
                 .scaleEffect(scale)
 //                .offset(x: proxy.size.width/2, y: proxy.size.height/2)
